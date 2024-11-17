56448ae6650dea423f2274ae1247b26cd32b5c94
Ansul Joshi <141168283+Ansul-Joshi@users.noreply.github.com>
2024-11-17 16:46:32 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,39 @@
+// primitive datatype
+//primitive data type while processing as copy is given
+// there are 7 categories
+//string  , Number , Boolean , null , undefined , Symbol , BigInt .
+
+// const score = 100
+// const scorevalue = 100.3
+// const isloged = false
+// const outsideTemp = null
+// let email;
+
+
+// Symbol give unique value and two variable with same value is not same 
+// const id = Symbol('123')
+// const id2 = Symbol('123')
+
+// console.log(id === id2);  // false
+
+// const bigNumber = 123456789876587655n
+
+//Reference type data type / Non primitive data type
+//refrence of the data is given for processing
+
+// Array , Objects , functions
+//js is dynamically typed 
+
+const heros = ["shaktiman" , "naagraj" ,"doga"]
+let myobj={
+    name:"Ansul",
+    age:20 ,
+}
+
+const myFunction = function(){
+console.log("hello world");
+}
+
+//type of Non-primitive data type is function
+
+
