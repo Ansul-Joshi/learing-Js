e63503be1b124b26d5ab3d108285dd31a6e66e3c
Ansul Joshi <141168283+Ansul-Joshi@users.noreply.github.com>
2024-11-17 16:25:21 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,57 @@
+// let score =null
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueno = Number(score);
+// console.log(typeof valueno)
+// console.log(valueno)
+
+//"33" => 33
+// "33abc" => NaN
+//true => 1 false => 0
+
+// let islogin =""
+// let bislogin = Boolean(islogin);
+// console.log(bislogin)
+
+// 1 true
+// 0 false
+// "string" = true
+// "" = false
+
+// let sn = 33
+// let ssn = String(sn)
+// console.log(ssn)
+// console.log(typeof ssn)
+
+//**********************************OPERATION***********************************************/
+
+// let value =3 
+// let negvalue = -value;
+// console.log(negvalue)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+// let str1 = "hello"
+// let str2 = "ansul"
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 2) //12
+// console.log(1 + "2")//12
+//console.log("1" + 2 + 2)  //122
+//console.log(1+2+"2")  //32
+
+//console.log(+true); //1
+//console.log(+""); //0
+
+let n1 , n2 , n3
+n1 = n2 = n3 = 2+2
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,14 @@
-console.log("Ansul");
+const accountId = 144553
+let accEmail = "Ansul@gmail.com"
+var accPass = "12345"
+accCity = "Jaipur"
+let accState;
+//accountId = 2 // not allowed
+
+accEmail="Joshi@gmail.com"
+accPass = "1223232"
+accCity = "mumbai"
+/*dont use var use let to decalre variable */
+// console.table([accountId,accEmail,accPass,accCity,accState]);
+console.log(typeof(accEmail));
+console.log(typeof null);
