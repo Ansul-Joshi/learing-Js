05091000e696369f17f3af9c000d4e10265bf807
Ansul Joshi <141168283+Ansul-Joshi@users.noreply.github.com>
2024-11-17 16:25:41 +0000

    comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2=1);
+
+// console.log("2">1); //true converts
+// console.log("02">1); // true consverts
+
+// console.log(null>0); // f
+// console.log(null==0); // f
+// console.log(null>=0); //t  convert null into 0 and check the condition
+
+// console.log(undefined>0); // f
+// console.log(undefined==0); // f
+// console.log(undefined>=0); //f
+
+//in js == and === works differently and > works differently
+
+// === :- it checks datatype and checks the values
+
+console.log("2"===2); // f because dt is different
+
+
+
+
+
