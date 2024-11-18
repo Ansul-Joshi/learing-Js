2eabf25e7130a445cac74804f87620519d60d4e4
Ansul Joshi <141168283+Ansul-Joshi@users.noreply.github.com>
2024-11-18 17:43:00 +0000

    fkmvmsofv

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -34,6 +34,35 @@ const myFunction = function(){
 console.log("hello world");
 }
 
-//type of Non-primitive data type is function
+//type of Non-primitive data type is object
+
+/*****************************************************************************/
+
+//there are two types of memeory used in js
+// Stack memeory is use in primitive datatype
+//Heap memory is used in non-prmitive datatype
+
+let myname = "Ansul Deepak Joshi"
+
+let mymyname = myname
+mymyname = "Joshi Ansul Deepak"
+console.log(myname);
+console.log(mymyname);
+
+let user1 ={
+    email : "user@gmail.com",
+    upi : "user@bbl"
+}
+
+let user2 =user1
+
+user2.email = "Ansul@gmail.com";
+
+console.log(user1);
+console.log(user2);
+
+
+//stack mai copie bantha hai tho chages different retha hai agar (change in one variable will not effect another variable)
+// heap mai 2 different variables ko same same value ka reference miltha hai (change in one variable will effect another variable)
 
 
---
+++ 01_basics/strings.js
@@ -0,0 +1,34 @@
+const name  = "name"
+const repoCount = 50
+
+console.log(name + repoCount + "value");
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`); //sting interpolation
+
+const gamename = new String('Ansul-joshi') // index deta hai 
+console.log(gamename)
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('s'));
+
+const newstr = gamename.substring(0,4)
+console.log(newstr);
+const astr = gamename.slice(-8,4)
+console.log(astr);
+const newStringOne = "  Ansul   "
+console.log(newStringOne)
+console.log(newStringOne.trim());
+
+
+const url = "https://Ansul.com/ansul%20Joshi"
+
+console.log(url.replace('%20','_'));
+
+console.log(url.includes('https'));
+
+console.log(gamename.split('-'));
+
