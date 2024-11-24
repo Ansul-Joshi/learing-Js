8dbff6c068ab9f9874736730b5072b0aa079e4d4
Ansul Joshi <141168283+Ansul-Joshi@users.noreply.github.com>
2024-11-24 13:38:07 +0000

    API

--- 02_basics/objectlast.js
+++ 02_basics/objectlast.js
@@ -1,2 +1,34 @@
 //de-structuring
 
+const course ={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"Ansul"
+}
+
+//couserse.couseinstrctor
+
+const {courseInstructor : instructor} = course
+
+console.log(instructor);
+
+// const navbar = {{company}} {
+
+// }
+
+// navbar(company = "Ansul")
+
+
+//APIs
+
+// {
+//     "name" : "Ansul",
+//     "coursename" :"Js in hindi",
+//     "price" :"free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
